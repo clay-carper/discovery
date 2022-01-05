d4aac1d6f2132feb71c13ffb1b99c3f6639b57db
Clay Carper <ccarper2@uwyo.edu>
2022-01-05 11:06:16 -0700

    My solution to the challenge

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,21 +2,23 @@
 #![no_main]
 #![no_std]
 
-use volatile::Volatile;
+//use volatile::Volatile;
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! { // This type indicates the function can't return i.e. it will never terminate
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let half_period = 50_u16;
+    //let v_half_period = Volatile::new(&mut half_period);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
-
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+        for i in 0..8 {
+            leds[i].on().ok();
+            delay.delay_ms(half_period);
+            leds[i].off().ok();
+            delay.delay_ms(half_period/10); 
+        }
+        
     }
 }
