ff8df9ea81a0210e300a3b1a3d486ec7d506ed69
Clay Carper <ccarper2@uwyo.edu>
2022-01-07 13:11:12 -0700

    Spooky action at a distance

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -4,14 +4,47 @@
 use core::ptr;
 
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprintln, ITM};
+
+// Print the current contents of odr
+fn iprint_odr(itm: &mut ITM) {
+    const GPIOE_ODR: u32 = 0x4800_1014;
+
+    unsafe {
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let mut itm= aux7::init().0;
 
     unsafe {
-        ptr::read_volatile(0x4800_1800 as *const u32);
+        // A magic addresses!
+        const GPIOE_BSRR: u32 = 0x4800_1018;
+
+        // Print the initial contents of ODR
+        iprint_odr(&mut itm);
+
+        // Turn on the "North" LED (red)
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        iprint_odr(&mut itm);
+
+        // Turn on the "East" LED (green)
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        iprint_odr(&mut itm);
+
+        // Turn off the "North" LED
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        iprint_odr(&mut itm);
+
+        // Turn off the "East" LED
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        iprint_odr(&mut itm);
     }
 
     loop {}
