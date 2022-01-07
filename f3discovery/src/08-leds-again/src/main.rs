2720f9be0e9784f7fbf5d636c9405fbc2cc86e34
Clay Carper <ccarper2@uwyo.edu>
2022-01-07 15:38:30 -0700

    Worked through LEDs, again

--- f3discovery/src/08-leds-again/src/main.rs
+++ f3discovery/src/08-leds-again/src/main.rs
@@ -8,7 +8,20 @@ use aux8::entry;
 fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
-    // TODO initialize GPIOE
+    // enable the GPIOE peripheral
+    rcc.ahbenr.write(|w| w.iopeen().set_bit());
+
+    // configure the pins as outputs
+    gpioe.moder.write(|w| {
+        w.moder8().output();  // LD3 | PE8 Blue, NW light
+        w.moder9().output();  // LD4 | PE9 Red N light
+        w.moder10().output(); // LD5 | PE10 Orange NE light
+        w.moder11().output(); // LD7 | PE11 Green E light 
+        w.moder12().output(); // LD9 | PE12 Blue SE light
+        w.moder13().output(); // LD10 | PE13 Red S light
+        w.moder14().output(); // LD8 | PE14 Orange SW light
+        w.moder15().output() // LD6 | PE15 Green W light
+    });
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
