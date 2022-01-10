26de60fedae647c569805e913d02024d0ac7b676
Clay Carper <ccarper2@uwyo.edu>
2022-01-10 12:32:12 -0700

    Slowed down processor, correct solution

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -1,4 +1,3 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -12,6 +11,9 @@ fn main() -> ! {
     let instant = mono_timer.now();
     // Send a single character
     for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+        // wait until it's safe to write to TDR
+        while usart1.isr.read().txe().bit_is_clear() {}
+
         usart1
             .tdr
             .write(|w| w.tdr().bits(u16::from(*byte)));
