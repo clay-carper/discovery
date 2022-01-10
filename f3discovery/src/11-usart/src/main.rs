e28e392497c5c367cb75e01c5094907256c238ae
Clay Carper <ccarper2@uwyo.edu>
2022-01-10 13:08:12 -0700

    Added impl fmt::Write for SerialPort (complete)

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -1,31 +1,54 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+            // wait until it's safe to write to TDR
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+            // Write a byte
+            self
+                .usart1
+                .tdr
+                .write(|w| w.tdr().bits(u16::from(*byte)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
     let (usart1, mono_timer, mut itm) = aux11::init();
 
-    let instant = mono_timer.now();
-    // Send a single character
-    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
-        // wait until it's safe to write to TDR
-        while usart1.isr.read().txe().bit_is_clear() {}
+    let mut serial = SerialPort { usart1 };
 
-        usart1
-            .tdr
-            .write(|w| w.tdr().bits(u16::from(*byte)));
-    }
-    let elapsed = instant.elapsed();
-
-    iprintln!(
-        &mut itm.stim[0],
-        "`for` loop took {} ticks ({} us)",
-        elapsed,
-        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
-    );
+    uprintln!(serial, "The answer {}", 40 + 2);
+    
 
     loop {}
 }
