56c0c1c44ca002a522d5b45b724713d9f4016966
Clay Carper <ccarper2@uwyo.edu>
2022-01-10 12:15:43 -0700

    Initial sol., added a timer to investigate issue

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -6,12 +7,23 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
+    let instant = mono_timer.now();
     // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+        usart1
+            .tdr
+            .write(|w| w.tdr().bits(u16::from(*byte)));
+    }
+    let elapsed = instant.elapsed();
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
