0a0a540f807c4b7a7c2ff3c256270a114ea8b0ac
dr-brutalov <ccarper2@uwyo.edu>
2022-09-22 09:45:17 -0600

    Added some justification for structures

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -4,8 +4,9 @@
 
 use aux5::entry;
 
-#[entry]
-fn main() -> ! {
+#[entry] // from cortex-m-rt crate
+fn main() -> ! { // fn () -> doesn't return, ! in Rust is the never type (empty type in type theory)
+    // not a traditional main function, this is just our entry point for the microcontroller, can name it anything we want to.
     let _y;
     let x = 42;
     _y = x;
@@ -13,3 +14,6 @@ fn main() -> ! {
     // infinite loop; just so we don't leave this stack frame
     loop {}
 }
+
+// run with `cargo build --target thumbv7em-none-eabihf`
+// Run GDB with `gdb-multiarch -q -ex "target remote :3333" target/thumbv7em-none-eabihf/debug/led-roulette`
