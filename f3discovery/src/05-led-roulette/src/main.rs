f664c2a3efb38066b3bce36191c60921107e927b
dr-brutalov <ccarper2@uwyo.edu>
2022-09-22 11:12:36 -0600

    Example volatile to allow gdb manipulation

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,20 +2,22 @@
 #![no_main]
 #![no_std]
 
+use volatile::Volatile;
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry] // from cortex-m-rt crate
 fn main() -> ! { // fn () -> doesn't return, ! in Rust is the never type (empty type in type theory)
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let half_period = 500_u16;
+    let mut half_period = 500_u16;
+    let v_half_period = Volatile::new(&mut half_period);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(half_period);
+        leds[4].on().ok();
+        delay.delay_ms(v_half_period.read());
 
-        leds[0].off().ok();
-        delay.delay_ms(half_period);
+        leds[4].off().ok();
+        delay.delay_ms(v_half_period.read());
     }
 }
 
