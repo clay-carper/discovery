d5749aacc1ed48d6314029a74cd09e95916a17ae
dr-brutalov <ccarper2@uwyo.edu>
2022-09-22 11:40:54 -0600

    Initial led roulette implementation

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,22 +2,28 @@
 #![no_main]
 #![no_std]
 
-use volatile::Volatile;
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry] // from cortex-m-rt crate
 fn main() -> ! { // fn () -> doesn't return, ! in Rust is the never type (empty type in type theory)
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let half_period = 50_u16;
 
     loop {
-        leds[4].on().ok();
-        delay.delay_ms(v_half_period.read());
 
-        leds[4].off().ok();
-        delay.delay_ms(v_half_period.read());
+        for index in 0..8 { // make a new variable to index over
+            let next = (index + 1) % 8; // increment the index, and pass it to a new var to address the arrays
+                                               // Note: my initial attempt I skipped this step, but due to the difference
+                                               //       in typing and how the value is passed to leds, it won't work thatta way
+            
+            leds[next].on().ok();
+            delay.delay_ms(half_period);
+
+            leds[next].off().ok();
+            delay.delay_ms(half_period);
+        }
+        
     }
 }
 
