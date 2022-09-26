f225652ee1f7c94b548583bb06d7afcd54347580
dr-brutalov <ccarper2@uwyo.edu>
2022-09-26 15:34:55 -0600

    Used volatile to ensure release has desired elems

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
@@ -13,16 +15,16 @@ fn main() -> ! {
         const GPIOE_BSRR: u32 = 0x48001018;
 
         // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9; // Writing 1 << 9 (`BS9 = 1`) to `BSRR`  sets `PE9 high`; turns on the North LED on
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9); // Writing 1 << 9 (`BS9 = 1`) to `BSRR`  sets `PE9 high`; turns on the North LED on
 
         // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11; // Writing 1 << 11 (`BS11 = 1`) sets `PE11 high`; turns the East LED on
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11); // Writing 1 << 11 (`BS11 = 1`) sets `PE11 high`; turns the East LED on
 
         // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16); // Writing 1 << 25 (`BS9 = 1`) to `BSRR`  sets `PE9 lwo`; turns on the North LED off
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16)); // Writing 1 << 25 (`BS9 = 1`) to `BSRR`  sets `PE9 lwo`; turns on the North LED off
 
         // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16); // Writing 1 << 27 (`BS11 = 1`) sets `PE11 high`; turns the East LED off
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16)); // Writing 1 << 27 (`BS11 = 1`) sets `PE11 high`; turns the East LED off
     }
 
     loop {}
