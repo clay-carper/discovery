c8cfd245c79068ed9cc229b54728f94fa2c8fd36
dr-brutalov <ccarper2@uwyo.edu>
2022-10-04 10:55:34 -0600

    Initial comments

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -8,7 +8,8 @@ use aux11::{entry, iprint, iprintln};
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    // Send a single character
+    // Send a single character (one byte) by writing to the `TDR` register.
+    // This causes the `USART` peripheral to send one byte of information through the serial interface
     usart1
         .tdr
         .write(|w| w.tdr().bits(u16::from(b'X')) );
