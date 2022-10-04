4d3a2ede863bbe1c3bbbc59f68c2084bf04db04d
dr-brutalov <ccarper2@uwyo.edu>
2022-10-04 15:21:41 -0600

    Example of receiving a single byte

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -7,27 +7,15 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, mut itm) = aux11::init();
+    let (usart1, _mono_timer, _itm) = aux11::init();
 
-    let instant = mono_timer.now();
-    
-    // Send the string
-    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
-        while usart1.isr.read().txe().bit_is_clear() {} // Use the TXE flag of the ISR status register to slow things down
+    loop {
+        // Wait until there's data available
+        while usart1.isr.read().rxne().bit_is_clear() {}
 
-        usart1
-            .tdr
-            .write(|w| w.tdr().bits(u16::from(*byte)));
+        // Retrieve the data
+        let _byte = usart1.rdr.read().rdr().bits() as u8;
+
+        aux11::bkpt();
     }
-    let elapsed = instant.elapsed(); // in ticks
-
-    iprintln!(
-            &mut itm.stim[0],
-            "`for` loop took {} ticks ({} us)",
-            elapsed,
-            elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
-    );
-    
-
-    loop {}
 }
