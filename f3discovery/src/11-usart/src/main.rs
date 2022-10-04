23f9b87c2c20dbf4e2011687fd4d81edef5aede2
dr-brutalov <ccarper2@uwyo.edu>
2022-10-04 14:32:00 -0600

    Timing to show buffer overrun

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -6,17 +7,24 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character (one byte) by writing to the `TDR` register.
-    // This causes the `USART` peripheral to send one byte of information through the serial interface
-    // Works as intended when using `next` to step through. However, both `continue` and running in 
-    // release mode give a jumbled string. Less than ideal, for sure.
+    let instant = mono_timer.now();
+    
+    // Send the string
     for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
         usart1
             .tdr
             .write(|w| w.tdr().bits(u16::from(*byte)) );
     }
+    let elapsed = instant.elapsed(); // in ticks
+
+    iprintln!(
+            &mut itm.stim[0],
+            "`for` loop took {} ticks ({} us)",
+            elapsed,
+            elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
     
 
     loop {}
